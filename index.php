049660a36d8359162ff049cee93a3df6b5a1d5e0
Hasitha Charaka Gangoda <hasithacharaka@gmail.com>
2021-06-05 12:14:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
+$path = "https://api.telegram.org/bot1477242844:AAEnliAp5u8r31HuD9guaLVZKexhYpUZ2ZU";
 
+$update = json_decode(file_get_contents("php://input"), TRUE);
 
-
-echo ('hello');
-
-
-
-
-
-
-
-
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ if($message){
+ file_get_contents($path."/sendmessage?chat_id=".$chatId."under construction");
 
 ?>
